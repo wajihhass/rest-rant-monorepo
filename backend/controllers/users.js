6c32d3352b94546c37432c93576320534b847724
Wajih <hass_wajih@hotmail.com>
2022-05-26 12:09:39 -0400

    9.3.6. 9.3.10

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,18 +3,15 @@ const db = require("../models")
 const bcrypt = require('bcrypt')
 const { User } = db
 
-/*router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
     let { password, ...rest } = req.body;
     const user = await User.create({
-        ...rest, password_diqest: await bcrypt.hash(password, 10)
+        ...rest, 
+        password_diqest: await bcrypt.hash(password, 10)
     })
     res.json(user)
-})*/
-
-router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
-    res.json(user)
 })
+
 
 router.get('/', async (req, res) => {
     const users = await User.findAll()
